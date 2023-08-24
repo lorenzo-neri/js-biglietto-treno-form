3d97501518bf31b5c0778194970542c65d20a78f
lorenzo-neri <lorenzo21neri@gmail.com>
2023-08-24 14:30:19 +0200

    add structure in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,49 @@ Il recap dei dati e l'output del prezzo finale, andranno quindi stampati in pagi
 Questo richiederà un minimo di ricerca.
 */
 
+/* 
+tools
+- prompt
+- Number
+- const/let
+- Math.flor/Math.ceil
+- if/else
+- console.log
+*/
+
+//chiedo il numero di km da percorrere
+const kmPercorsi = Number(prompt('Ciao, quanti chilometri devi percorrere?'));
+console.log(kmPercorsi);
+
+//chiedo quanti anni ha il passeggero
+const etaPasseggero = Number(prompt('Quanti anni hai?'));
+console.log(etaPasseggero);
+
+//calcolo il prezzo del biglietto sulla base di 0.21 € al km
+if (kmPercorsi > 0 && etaPasseggero > 0) {
+    let prezzoBiglietto = (kmPercorsi * 0.21);
+    console.log(prezzoBiglietto.toFixed(2));
+
+    //i minorenni pagano il 20% in meno
+    if (etaPasseggero < 18) {
+        prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto * 20) / 100);
+        console.log(prezzoBiglietto.toFixed(2));
+
+        //gli over 65 pagano il 40% in meno
+    } else if (etaPasseggero >= 65) {
+        prezzoBiglietto = prezzoBiglietto - ((prezzoBiglietto * 40) / 100);
+        console.log(prezzoBiglietto.toFixed(2));
+    }
+
+    //stampo il prezzo finale con un massimo di due cifre decimali (centesimi)
+
+    const divPrezzoBiglietto = document.getElementById('prezzo_biglietto');
+    console.info(divPrezzoBiglietto);
+
+    divPrezzoBiglietto.innerHTML = 'Questo è l\'importo da pagare per il biglietto: ' + prezzoBiglietto.toFixed(2) + ' €';
+
+    /* prezzoBiglietto.innerHTML = `Questo è l'importo da pagare per il biglietto: ${prezzoBiglietto.toFixed(2) + '€'} `; */
+} else {
+    alert('Dati errati o mancanti');
+    location.reload();
+}
